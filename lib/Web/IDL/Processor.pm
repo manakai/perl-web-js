caa2ba985f509a9361e4cd5e4510f0c43faf25fc
Wakaba <wakaba@suikawiki.org>
2014-08-03 19:33:16 +0900

    notes on new reqs

--- lib/Web/IDL/Processor.pm
+++ lib/Web/IDL/Processor.pm
@@ -742,6 +742,7 @@ sub _extended_attributes ($$$$$) {
           }
           $dest->{_exposed} = $attr->{value_names} || [];
           # XXX MUST NOT be specified for exception's const
+          # XXX MUST be subset of interface's exposure set
         } else { # interface
           if ($src->{partial}) {
             $dest_context->{Exposed} = $attr->{value_names} || [];
@@ -751,6 +752,7 @@ sub _extended_attributes ($$$$$) {
           }
         }
         # XXX for dictionary, there MUST be Constructor
+        # XXX exposure sets MUST be subset of consequential's exposure sets
         next;
       }
     }
@@ -1495,7 +1497,7 @@ sub end_processing ($) {
       }
     }
   } # idl_defs
-  # XXX warn if interfaces in {implements} are not Exposed to same set
+  # XXX error if interfaces in {implements} are not Exposed to same set
   # of globals
 
   ## Interface objects
