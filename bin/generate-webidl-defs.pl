3a8c796c0301dc44007a9c9c23373f496bc1d252
Wakaba <wakaba@suikawiki.org>
2016-12-27 00:18:33 +0900

    stability

--- bin/generate-webidl-defs.pl
+++ bin/generate-webidl-defs.pl
@@ -13,7 +13,7 @@ my $data_path = path (__FILE__)->parent->parent->child ('local/webidl.json');
 my $data = json_bytes2perl $data_path->slurp;
 $Data->{$_} = $data->{$_} for qw(keyword_tokens other_tokens);
 
-for (keys %{$data->{keyword_tokens}}) {
+for (sort { $a cmp $b } keys %{$data->{keyword_tokens}}) {
   if ($data->{keyword_tokens}->{$_}->{argument_name}) {
     push @{$Data->{grammer}->{ArgumentNameKeyword}->{patterns} ||= []},
         [{type => $_, set_type => 'name'}];
@@ -24,7 +24,7 @@ for (keys %{$data->{keyword_tokens}}) {
   }
 }
 
-for my $construct (keys %{$data->{constructs}}) {
+for my $construct (sort { $a cmp $b } keys %{$data->{constructs}}) {
   $Data->{reserved}->{$construct} = $data->{constructs}->{$construct}->{reserved}
       if $data->{constructs}->{$construct}->{reserved};
   $Data->{allowed_xattrs}->{$construct} = $data->{constructs}->{$construct}->{allowed_extended_attributes}
@@ -32,10 +32,10 @@ for my $construct (keys %{$data->{constructs}}) {
 }
 
 my $disallowed_combinations = {};
-for my $name (keys %{$data->{extended_attributes}}) {
+for my $name (sort { $a cmp $b } keys %{$data->{extended_attributes}}) {
   $Data->{xattr_args}->{$name} = $data->{extended_attributes}->{$name}->{args};
   $Data->{xattr_multiple}->{$name} = 1 if $data->{extended_attributes}->{$name}->{multiple};
-  for my $name2 (keys %{$data->{extended_attributes}->{$name}->{disallowed_extended_attributes}}) {
+  for my $name2 (sort { $a cmp $b } keys %{$data->{extended_attributes}->{$name}->{disallowed_extended_attributes}}) {
     my @name = sort { $a cmp $b } $name, $name2;
     $disallowed_combinations->{$name[0], $name[1]} = \@name;
   }
